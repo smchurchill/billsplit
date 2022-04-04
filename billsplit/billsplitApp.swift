b7e46dd3065e22d67b9dbcce70c57b4823752fd9
Samuel Churchill <meadowschurchill@gmail.com>
2022-04-04 12:58:09 -0700

    Initial Commit

---
+++ billsplit/billsplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  billsplitApp.swift
+//  billsplit
+//
+//  Created by sam on 2022-04-04.
+//
+
+import SwiftUI
+
+@main
+struct billsplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
